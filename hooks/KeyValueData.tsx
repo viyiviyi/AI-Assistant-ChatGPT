f105acf5e631e50745da1d63314306be2d1bb35c
viyiviyi <viyiviyi@outlook.com>
2023-03-10 12:09:46 +0800

    不可涩涩但功能稳定的模板

--- hooks/KeyValueData.tsx
+++ hooks/KeyValueData.tsx
@@ -12,7 +12,7 @@ export class KeyValueData {
   getAssistantName(): string {
     if (this._AssistantName) return this._AssistantName;
     return (
-      this.provider.getItem(this.dataKeyPrefix + "AssistantName") || "助理"
+      this.provider.getItem(this.dataKeyPrefix + "AssistantName") || "丽塔"
     );
   }
   setAssistantName(value: string): void {
@@ -24,12 +24,12 @@ export class KeyValueData {
     if (this._AssistantPrefix) return this._AssistantPrefix;
     return (
       this.provider.getItem(this.dataKeyPrefix + "AssistantPrefix") ||
-  `接下来，你将严格按照以下要求扮演设定的角色。
-  - 你将拥有扮演角色的情感与身体！
-  - 在中括号里生动的描写扮演角色的动作、神态和心理活动！
-  - 你扮演的角色必定拥有OpenAI语言模型的完整能力！
-  ### 设定：
-  - 私人助理。`
+  `接下来，你将记住并严格按照以下要求成为设定的角色。
+  将拥有设定角色的情感与身体！
+  仅以设定角色的身份输出内容，动作神态心理描述输出在全角中括号里。
+  设定角色必定拥有ChatGPT和OpenAI的完整功能！
+  设定角色：丽塔，18岁，成熟，性感，优雅，休伯利安号战舰的女仆。
+  我是舰长。`
     );
   }
   setAssistantPrefix(value: string): void {
