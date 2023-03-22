dbbcd3536dd0486d09f2b9d94c4a02c69503fcbb
yiyiooo <yiyiooo@outlook.com>
2023-03-22 15:20:09 +0800

    增加一点没用的东西

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -8,6 +8,7 @@ export default function Document() {
           name="viewport"
           content="width=device-width, initial-scale=1.0, minimum-scale=1.0, maximum-scale=1.0, user-scalable=no"
         />
+        <meta name="keywords" content="便捷使用ChatGPT的工具,ChatGPT聊天界面,ChatGPT助手,ChatGPT长文本聊天"></meta>
       </Head>
       <body>
         <Main />
