d4474a1d28be7723ae4a9f8e92e3a3942ab571f5
viyiviyi <viyiviyi@outlook.com>
2023-05-07 14:45:47 +0800

    改了404页面的内容

--- pages/404.tsx
+++ pages/404.tsx
@@ -1,10 +1,24 @@
+import Image from "next/image";
 import { useRouter } from "next/router";
 import { useEffect } from "react";
+import logo from "../public/logo.png";
 
 export default function Custom404() {
   const router = useRouter();
   useEffect(() => {
     router.replace("/");
-  });
-  return <div></div>;
+  }, [router]);
+  return (
+    <div
+      style={{
+        display: "flex",
+        width: "100%",
+        height: "100%",
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <Image src={logo} alt="" width={100} height="100"></Image>
+    </div>
+  );
 }
