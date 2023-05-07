33b4b899592abb2c0f41de45c6992c0d22026cbc
viyiviyi <viyiviyi@outlook.com>
2023-05-07 14:24:26 +0800

    qwe

---
+++ pages/404.tsx
@@ -0,0 +1,10 @@
+import { useRouter } from "next/router";
+import { useEffect } from "react";
+
+export default function Custom404() {
+  const router = useRouter();
+  useEffect(() => {
+    router.replace("/");
+  });
+  return <div></div>;
+}
