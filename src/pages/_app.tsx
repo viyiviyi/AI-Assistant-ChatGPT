b96ed0167cfe7d70d30bf06a17443edfe65ef984
yiyiooo <yiyiooo@outlook.com>
2023-06-02 12:51:45 +0800

    解决只兼容新版浏览器的问题

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,4 +1,6 @@
 import { useDark } from "@/core/hooks";
+import { StyleProvider, legacyLogicalPropertiesTransformer } from "@ant-design/cssinjs";
+
 import "@/styles/globals.css";
 import { ConfigProvider, theme } from "antd";
 import "antd/dist/reset.css";
@@ -14,32 +16,34 @@ export default function App({ Component, pageProps }: AppProps) {
     setOrgin(location.origin);
   }, []);
   return (
-    <ConfigProvider
-      theme={{
-        token: {
-          colorPrimary: "#00b96b",
-          colorInfoBg: isDark ? "#444a" : "#ddda",
-          colorTextBase: isDark ? "#ddd" : "#333",
-        },
-        algorithm: isDark ? theme.darkAlgorithm : theme.compactAlgorithm,
-      }}
-    >
-      <Head>
-        <meta
-          name="viewport"
-          content="width=device-width, initial-scale=1.0, minimum-scale=1.0, maximum-scale=1.0, user-scalable=no"
-        />
-        {orgin == "https://22733.site" ? (
-          <script
-            async
-            src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-7936769145776010"
-            crossOrigin="anonymous"
-          ></script>
-        ) : (
-          <></>
-        )}
-      </Head>
-      <Component {...pageProps} />
-    </ConfigProvider>
+    <StyleProvider hashPriority="high" transformers={[legacyLogicalPropertiesTransformer]}>
+      <ConfigProvider
+        theme={{
+          token: {
+            colorPrimary: "#00b96b",
+            colorInfoBg: isDark ? "#444a" : "#ddda",
+            colorTextBase: isDark ? "#ddd" : "#333",
+          },
+          algorithm: isDark ? theme.darkAlgorithm : theme.compactAlgorithm,
+        }}
+      >
+        <Head>
+          <meta
+            name="viewport"
+            content="width=device-width, initial-scale=1.0, minimum-scale=1.0, maximum-scale=1.0, user-scalable=no"
+          />
+          {orgin == "https://22733.site" ? (
+            <script
+              async
+              src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-7936769145776010"
+              crossOrigin="anonymous"
+            ></script>
+          ) : (
+            <></>
+          )}
+        </Head>
+        <Component {...pageProps} />
+      </ConfigProvider>
+    </StyleProvider>
   );
 }
