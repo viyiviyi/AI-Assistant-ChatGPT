e75acaeb2f907496990b499cb105d8d5920899ab
yiyiooo <yiyiooo@outlook.com>
2023-08-31 09:39:06 +0800

    增加manifest.json 文件

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -6,6 +6,7 @@ const MyDocument = () => (
   <Html lang="zh-cn">
     <Head>
       <link rel="icon" href="/favicon.ico" />
+      <link rel="manifest" href="/manifest.json" />
       <meta
         name="keywords"
         content="便捷使用ChatGPT,ChatGPT工具,ChatGPT聊天界面,ChatGPT助手,ChatGPT长文本聊天,自由的ChatGPT客户端,AI助理,Claude使用工具,OpenAI,ChatGPT,ChatGPT网页版,ChatGPT创作工具,ChatGPT写文工具,ChatGPT,AI创作工具,AI助理,AI机器人,ChatGPT机器人,ChatGPT插件,AI插件,大语言模型,大语言模型使用,ChatGPT对话,AI对话,完全免费,免费工具,免费ChatGPT客户端,免费客户端,"
